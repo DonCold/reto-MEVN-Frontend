0803319413f1ed1893d2c693dbd8cc4e1e594be2
jeisson botache <jeisson228@hotmail.com>
2020-12-21 19:14:31 -0500

    mod router y creacion component
    no es mucho, sólo conecté mi pagina

---
+++ src/components/Categorias.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <br>
+    <div class="alert alert-primary" role="alert">
+        <h2>CATEGORIAS</h2>
+
+        
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+      name: "CategoriasComponent"
+  }
+</script>
+
+<style>
+
+</style>
